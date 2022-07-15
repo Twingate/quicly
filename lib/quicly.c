2445ab66035dc9e88f5f4de92da42502278fb410
Dmitry Adamushko <da@stablebits.net>
2022-07-15 11:39:15 +0200

    Fixing windows build.

--- lib/quicly.c
+++ lib/quicly.c
@@ -22,13 +22,9 @@
 #include "compat.h"
 #include <assert.h>
 #include <inttypes.h>
-#include <netinet/in.h>
-#include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/time.h>
 #include "khash.h"
 #include "quicly.h"
 #include "quicly/defaults.h"
